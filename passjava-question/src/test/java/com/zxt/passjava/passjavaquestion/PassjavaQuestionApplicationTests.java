c66fa054c5685c4d5e9975a32c1d8ca3ea4e55e6
zhouxingtao <1632561836@qq.com>
2021-12-09 17:02:53 +0800

    第三次提交

--- passjava-question/src/test/java/com/zxt/passjava/passjavaquestion/PassjavaQuestionApplicationTests.java
+++ passjava-question/src/test/java/com/zxt/passjava/passjavaquestion/PassjavaQuestionApplicationTests.java
@@ -5,16 +5,35 @@ import com.zxt.passjava.question.PassjavaQuestionApplication;
 import com.zxt.passjava.question.entity.TypeEntity;
 import com.zxt.passjava.question.service.TypeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.UUID;
 
 @SpringBootTest(classes = PassjavaQuestionApplication.class)
 class PassjavaQuestionApplicationTests {
 
     @Resource
     TypeService typeService;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void TestStringRedisTemplate() {
+        // 初始化 redis 组件
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        // 存储数据
+        ops.set("悟空", "悟空聊架构_" + UUID.randomUUID().toString());
+        // 查询数据
+        String wukong = ops.get("悟空");
+        System.out.println(wukong);
+    }
+
 
     // 创建题目类型
     @Test
